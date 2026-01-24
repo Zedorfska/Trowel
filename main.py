30cedaf4d08f169a92e7bd7ab8b0bbf5582514c3
Zvyezdana <Zedorfska@gmail.com>
2026-01-24 05:40:36 +0100

    touchups to CoC code

--- main.py
+++ main.py
@@ -437,28 +437,87 @@ async def clan(ctx):
 
 #
 
+# TODO: this
+#async def coc_print_war_members(members):
+
 @clan.command(name = "currentwar")
 async def clan_currentwar(ctx):
     response = requests.get("https://api.clashofclans.com/v1/clans/%232JJGGJR92/currentwar", headers = headers)
     war_json = response.json()
-    start_time = war_json["startTime"]
-    start_time_datetime = datetime.strptime(start_time, "%Y%m%dT%H%M%S.%fZ")
-    start_time_epoch = int(start_time_datetime.replace(tzinfo=timezone.utc).timestamp())
+    #TODO: make this a function
+    war_start_time = war_json["startTime"]
+    war_start_time_datetime = datetime.strptime(war_start_time, "%Y%m%dT%H%M%S.%fZ")
+    war_start_time_epoch = int(war_start_time_datetime.replace(tzinfo=timezone.utc).timestamp())
+    war_end_time = war_json["endTime"]
+    war_end_time_datetime = datetime.strptime(war_end_time, "%Y%m%dT%H%M%S.%fZ")
+    war_end_time_epoch = int(war_end_time_datetime.replace(tzinfo=timezone.utc).timestamp())
     members = war_json["clan"]["members"]
     opponents = war_json["opponent"]["members"]
+    members_sorted = sorted(members, key = lambda m: m["mapPosition"])
+    opponents_sorted = sorted(opponents, key = lambda m: m["mapPosition"])
     
-    message = ""
-
-    message += f"# {war_json['clan']['name']} vs {war_json['opponent']['name']}\n"
-    message += f"CURRENTLY IN {war_json['state'].upper()}\nWar starts <t:{start_time_epoch}:R>\n\n"
-    message += f"**Clan members:**\n"
+    member_with_longest_name = members[0]
     for member in members:
-        message += f"{member['name']}\n"
-    message += f"\n"
-    message += f"**Opponent clan members:**\n"
-    for member in opponents:
-        message += f"{member['name']}\n"
+        if len(member["name"]) > len(member_with_longest_name["name"]):
+            member_with_longest_name = member
 
+    message = ""
+    
+    if war_json["state"] == "preparation":
+        message += f"# {war_json['clan']['name']} vs {war_json['opponent']['name']}\n"
+        message += f"War starts <t:{war_start_time_epoch}:R>\n\n"
+    elif war_json["state"] == "inWar":
+        message += f"# {war_json['clan']['name']} [ {war_json['clan']['stars']} | {war_json['opponent']['stars']} ] {war_json['opponent']['name']}\n"
+        message += f"War ends <t:{war_end_time_epoch}:R>\n\n"
+    elif war_json["state"] == "notInWar":
+        message += "{war_json['clan']} is not making any war efforts."
+               
+    message += f"**{war_json['clan']['name']} clan members:**\n"
+
+    for member in members_sorted:
+        message += "`"
+        message += f"{member['mapPosition']}. "
+        if member["mapPosition"] < 10:
+            message += " "
+        message += f"TH{member['townhallLevel']}"
+        if member["townhallLevel"] < 10:
+            message += " "
+        message += " - "
+        message += f"{member['name']}"
+        
+        for i in range(len(member_with_longest_name["name"]) - len(member['name'])):
+            message += " "
+
+        member_attacks = 0
+        for attack in member.get("attacks", []):
+            member_attacks += 1
+
+        message += f" - {member_attacks}/2"
+        message += "`\n"
+
+    message += "\n"
+    message += f"**{war_json['opponent']['name']} clan members:**\n"
+
+    for member in opponents_sorted:
+        message += "`"
+        message += f"{member['mapPosition']}. "
+        if member["mapPosition"] < 10:
+            message += " "
+        message += f"TH{member['townhallLevel']}"
+        if member["townhallLevel"] < 10:
+            message += " "
+        message += " - "
+        message += f"{member['name']}"
+        
+        for i in range(len(member_with_longest_name["name"]) - len(member['name'])):
+            message += " "
+
+        member_attacks = 0
+        for attack in member.get("attacks", []):
+            member_attacks += 1
+
+        message += f" - {member_attacks}/2"
+        message += "`\n"
 
     await ctx.send(message)
 
